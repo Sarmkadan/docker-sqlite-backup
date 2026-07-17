bfdc0e5e95cc711701235babb65b8e448161253e
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 20:22:42 +0300

    refactor: small refactor - ChecksumUtilityTestsJsonExtensions

--- tests/docker-sqlite-backup.Tests/Utilities/ChecksumUtilityTestsJsonExtensions.cs
+++ tests/docker-sqlite-backup.Tests/Utilities/ChecksumUtilityTestsJsonExtensions.cs
@@ -1,18 +1,23 @@
 // Author: Vladyslav Zaiets
 
+using System.Globalization;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
 namespace DockerSqliteBackup.Tests.Utilities;
 
-public static class ChecksumUtilityTestsJsonExtensions
+/// <summary>
+    /// Provides JSON serialization and deserialization extensions for <see cref="ChecksumUtilityTests"/> instances.
+    /// </summary>
+    public static class ChecksumUtilityTestsJsonExtensions
 {
     private static readonly JsonSerializerOptions _jsonOptions = new(JsonSerializerDefaults.Web)
     {
         WriteIndented = false,
         PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
         DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
-        Converters = { new JsonStringEnumConverter(JsonNamingPolicy.CamelCase) }
+        Converters = { new JsonStringEnumConverter(JsonNamingPolicy.CamelCase) },
+        PropertyNameCaseInsensitive = true
     };
 
     /// <summary>
@@ -21,7 +26,7 @@ public static class ChecksumUtilityTestsJsonExtensions
     /// <param name="value">The instance to serialize.</param>
     /// <param name="indented">Whether to format the JSON with indentation.</param>
     /// <returns>A JSON string representation of the instance.</returns>
-    /// <exception cref="ArgumentNullException">Thrown if <paramref name="value"/> is null.</exception>
+    /// <exception cref="ArgumentNullException"><paramref name="value"/> is null.</exception>
     public static string ToJson(this ChecksumUtilityTests value, bool indented = false)
     {
         ArgumentNullException.ThrowIfNull(value);
@@ -38,6 +43,8 @@ public static class ChecksumUtilityTestsJsonExtensions
     /// </summary>
     /// <param name="json">The JSON string to deserialize.</param>
     /// <returns>The deserialized instance, or null if the JSON is empty or whitespace.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="json"/> is null.</exception>
+    /// <exception cref="ArgumentException"><paramref name="json"/> is empty or whitespace.</exception>
     /// <exception cref="JsonException">Thrown if the JSON is invalid or cannot be deserialized.</exception>
     public static ChecksumUtilityTests? FromJson(string json)
     {
@@ -57,6 +64,8 @@ public static class ChecksumUtilityTestsJsonExtensions
     /// <param name="json">The JSON string to deserialize.</param>
     /// <param name="value">Receives the deserialized instance if successful.</param>
     /// <returns>True if deserialization succeeded; otherwise, false.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="json"/> is null.</exception>
+    /// <exception cref="ArgumentException"><paramref name="json"/> is empty or whitespace.</exception>
     public static bool TryFromJson(string json, out ChecksumUtilityTests? value)
     {
         ArgumentException.ThrowIfNullOrEmpty(json);
