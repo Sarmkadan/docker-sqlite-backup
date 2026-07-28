9f60a76526b810f45f56e5dc5937347e21b5abe4
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-28 10:08:00 +0300

    BackupServiceTests.cs

---
+++ tests/docker-sqlite-backup.Tests/BackupServiceTests.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+using Moq;
+using DockerSqliteBackup.Tests;
+
+namespace DockerSqliteBackup.Tests
+{
+    public class BackupServiceTests
+    {
+        [Fact]
+        public async Task TestExecuteBackupAsync_Happy_PATH()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public async Task TestCalculateBackupChecksumAsync_Happy_PATH()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public async Task TestGetBackupHistoryAsync_Happy_PATH()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public async Task TestDeleteBackupAsync_Happy_PATH()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public async Task TestGetBackupResultAsync_Happy_PATH()
+        {
+            // Test implementation
+        }
+    }
+}
